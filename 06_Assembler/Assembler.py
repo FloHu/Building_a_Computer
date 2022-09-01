cd730f0579cdd1d72bec10d6a83215ed361769c7
Florian Huber <florian_huber@gmx.at>
2022-09-01 17:47:11 +0200

    Finished Assembler refactoring.

--- 06_Assembler/Assembler.py
+++ 06_Assembler/Assembler.py
@@ -11,44 +11,23 @@ class Assembler:
         outfile = basename + ".hack"
         outfile = open(outfile, "w")
         with open(infile, "r") as input:
-            nextline = input.readline()
-            decoded = self.parser(nextline)
+            for nextline in input:
+                decoded = self.parser.parse_nextline(nextline)
+                if decoded:
+                    print(f"Decoded nextline to {decoded}")
+                    outfile.write(decoded + '\n')
+        outfile.close()
 
 class Parser:
-    def __init__(self, infile):
-        # open file and read the first line
-        # currently assumes that empty lines mean EOF
-        self.infile = open(infile, "r")
-        basename, _ = os.path.splitext(infile)
-        outfile = basename + ".hack"
-        self.outfile = open(outfile, "w")
-
-    def advance(self):
-        nextline = self.infile.readline()
-        # skip empty lines
-        while nextline == '\n':
-            nextline = self.infile.readline()
-        if not nextline:
-            self.infile.close()
-            self.outfile.close()
-            self.hasMoreCommands = False
-            print("End of file reached, closing files.")
-        else:
-            self.hasMoreCommands = True
-        return nextline
-    
-    def parse_nextline(self):
-        nextline = self.advance()
-        # remove comments if present
+    def parse_nextline(self, nextline):
         p = re.compile('//.*')
         nextline = p.sub('', nextline)
         nextline = nextline.rstrip()
         nextline = nextline.rstrip(os.linesep)
+        decoded = ''
         if nextline:
-            #print("Found another line:", nextline)
             if nextline.startswith("@"):
                 commandType = "A_COMMAND"
-                #print(self.translate_mnemonics(nextline, commandType))
                 decoded = self.translate_mnemonics(nextline, commandType)
             elif nextline.startswith("("):
                 commandType = "L_COMMAND"
@@ -56,15 +35,11 @@ class Parser:
                 commandType = "C_COMMAND"
                 components = re.search("(?P<dest>[AMD]*)?(=)?(?P<comp>[01\-D!AM|+]+)(;)?(?P<jump>.*)?", nextline)
                 components = components.groupdict()
-                # TO DO: add check/tests if comp had no match or if there is only one of 3 components
                 for key, value in components.items():
                     if value == '':
                         components[key] = 'null'
-                #print(components)
-                #print(self.translate_mnemonics(components, commandType))
                 decoded = self.translate_mnemonics(components, commandType)
-            #print(commandType)
-            self.outfile.write(decoded + "\n")
+        return decoded
     
     def translate_mnemonics(self, components, command_type):
         decoded = ''
--- 06_Assembler/main.py
+++ 06_Assembler/main.py
@@ -1,16 +1,15 @@
 import sys
 import os
-from Assembler import Parser
+from Assembler import Assembler, Parser
 
 assert len(sys.argv) == 2, "Please provide exactly one file to be processed by the assembler."
 infile = sys.argv[1]
 assert os.path.exists(infile), f"Provided file {infile} does not exist"
 
 def main():
-    parser = Parser(infile)
-    parser.parse_nextline()
-    while parser.hasMoreCommands:
-        parser.parse_nextline()
+    parser = Parser()
+    assembler = Assembler(parser)
+    assembler.assemble(infile)
 
 if __name__ == '__main__':
     main()
